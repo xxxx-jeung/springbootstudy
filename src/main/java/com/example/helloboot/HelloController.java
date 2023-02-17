3862a34b26c196972cb3f600a31a422b16da9f31
xxxx-jeung <63729676+xxxx-jeung@users.noreply.github.com>
2023-02-17 13:37:07 +0900

    디스팩처 서블릿 및 애노테이션

--- src/main/java/com/example/helloboot/HelloController.java
+++ src/main/java/com/example/helloboot/HelloController.java
@@ -1,10 +1,34 @@
 package com.example.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Objects;
 
+
+/**
+ * 디스팩처 서블릿이 매핑 메소드를 일일이 찾을 수 있지만 그 만큼 시간이 오래걸린다.
+ * 따라서 매핑 클래스용 이라고 클래스에다 애노테이션으로 명시해 놓으면 디스팩처 서블릿이 매핑 클래스를 찾고 매핑 메소드를 찾아낸다.
+ */
+@RequestMapping("/hello")
 public class HelloController {
+  private final HelloService helloService;
+
+  public HelloController(HelloService helloService) {
+    this.helloService = helloService;
+  }
+
+  /**
+   * 서블릿 컨테이너인 웹 어플리케이션 컨텍스트를 생성자로 받았고, 디스팩처 서블릿이 Bean 을 다 뒤진다.
+   * 요청 -> 디스팩처 서블릿 -> 여기서 웹 요청을 처리할 수 있는 Bean 이 있는지 찾는다.
+   * @param name
+   * @return
+   */
+  @GetMapping
+  @ResponseBody
   public String hello(String name) {
-    SimpleHelloService simpleHelloService = new SimpleHelloService();
-    return simpleHelloService.sayHello(Objects.requireNonNull(name));
+    return helloService.sayHello(Objects.requireNonNull(name));
   }
 }
--- src/main/java/com/example/helloboot/HellobootApplication.java
+++ src/main/java/com/example/helloboot/HellobootApplication.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,44 +20,19 @@ import java.io.IOException;
 public class HellobootApplication {
 
   public static void main(String[] args) {
-    // 어플리케이션 컨텍스트 = 스프링을 대표하는 객체, 리소스 접근 방법, 내부 이벤트 수행, 이게 스프링 컨테이너가 된다.
-    // 스프링 컨테이너는 객체를 직접 만들어서 넣어주는것도 가능하지만 그것보다는 어떤 클래스를 이용해서 메타정보를 넣어주는게 일반적이다.
-    GenericApplicationContext applicationContext = new GenericApplicationContext();
-    applicationContext.registerBean(HelloController.class); // Bean 등록
-    applicationContext.refresh(); // Bean 오브젝트 생성
+    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(); // 어플리케이션 컨택스트의 종류는 매우 많다. 상황에 따라 바꿔가며 사용하면 되는데 디스팩쳐 서블릿을 사용하기 위해선 Web 어플리케이션 컨택스트를 사용해야 한다.
+    applicationContext.registerBean(HelloController.class);
+    applicationContext.registerBean(SimpleHelloService.class);
+    applicationContext.refresh();
 
     ServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
     WebServer webServer =
         tomcatServletWebServerFactory.getWebServer(
             servletContext -> {
-
+              // 프론트 컨트롤러 방식을 Spring 에서 제공하는 Servlet 을 사용하면 편리하다
               servletContext
                   .addServlet(
-                      "frontcontroller",
-                      new HttpServlet() {
-                        @Override
-                        protected void service(HttpServletRequest req, HttpServletResponse resp)
-                            throws ServletException, IOException {
-                          if (req.getRequestURI().equals("/hello")
-                              && req.getMethod().equals(HttpMethod.GET.name())) {
-                            String name = req.getParameter("name");
-
-                            HelloController helloController = applicationContext.getBean(HelloController.class); // 등록된 Bean 을 가져와 사용할 수 있다.
-                            String ret = helloController.hello(name);
-
-                            // 200번 코드가 아닌 다른 코드를 보여주고 싶을 때 사용하지만, 굳이 200번 말고 사용할 필요 없다면 지우는게 낫다., 성공하면 서블릿 컨테이너에서 200번대 코드를 내려준다.
-                            //resp.setStatus(HttpStatus.OK.value());
-                            //resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-
-                            resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                            resp.getWriter().println(ret);
-                          } else if (req.getRequestURI().equals("/user")) {
-                            //
-                          } else {
-                            resp.setStatus(HttpStatus.NOT_FOUND.value());
-                          }
-                        }
-                      })
+                      "dispatcherServlet ", new DispatcherServlet(applicationContext)) // 에러가 발생하는데, 디스팩처 서블릿에게 요청을 받을 때 어떤 컨트롤러에게 전달해줘야 하는지 힌트를 하나도 주지 않은 상태, 이러한 작업을 xml 로 했지만 지금은 @RequestMapping 을 통해 명시해준다.
                   .addMapping("/*");
             });
     webServer.start();

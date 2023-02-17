bdf33f0feb8fb13c0208de8da4249e19ddd85455
xxxx-jeung <63729676+xxxx-jeung@users.noreply.github.com>
2023-02-17 14:07:07 +0900

    자바 구성 정보

--- src/main/java/com/example/helloboot/HellobootApplication.java
+++ src/main/java/com/example/helloboot/HellobootApplication.java
@@ -3,27 +3,50 @@ package com.example.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
+/**
+ * 팩토리 메소드를 만들었으면 Bean 으로 등록한다.
+ * 스프링 컨테이너가 Bean 이 등록되었는지 알 수 없다.
+ * 따라서 개발자가 여기에 빈이 있다는 걸 알려주기 위해
+ * 클래스 위에 구성정보 애노테이션을 작성해놓으면
+ * 스프링 컨테이너에게 Bean 으로 등록할 메소드가 존재한다고 알려주는 것이다.
+ * 스프링 컨테이너는 그걸 알아듣고 Bean 을 등록해 놓는다.
+ *
+ * 여기서 중요한건 Configuration 이다
+ * 스프링 컨테이너에서 첫번째로 해당 애노테이션을 읽어들이기 때문이다.
+ * 스프링을 구성하는데 있어 제일 중요한 애노테이션 중 하나다.
+ */
+@Configuration
 public class HellobootApplication {
 
+  @Bean
+  public HelloController helloController(HelloService helloService) {
+    // 생성자에 주입을 해줘야 한다.
+    // 이걸 파라미터로 받아서 주입해준다.
+    // 자바의 팩토리 메소드
+    return new HelloController(helloService);
+  }
+
+  @Bean
+  public HelloService helloService() {
+    return new SimpleHelloService();
+  }
+
   public static void main(String[] args) {
-    // 수정 목적은 기존에는 스프링 컨테이너가 올라가고 나서 작업을 진행했지만,
-    // 스프링 컨테이너가 올라가는 도중에 개발자가 정의한 작업을 실행하도록 하려고 한다. (스프링 부트가 이렇게 구성되어있음)
-    // 스프링 컨테이너가 올라갈 때 부가적으로 실행해야 항목이 있다면 onRefresh() 안에 정의해 놓으면 된다.
-    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+    /**
+     * GenericWebApplicationContext 는 자바에서 만든 애노테이션을 읽어들일 수 없다.
+     * 따라서 AnnotationConfigWebApplicationContext 로 바꿔야 한다.
+     */
+    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+      // 스프링 컨테이너 안에 객체를 어떻게 넣을것인가, Bean 이라는 객체가 다른 객체를 의존 또는 주입을 언제 어떻게 할 것인지에 대해 알아보자
+      // 팩토리 메소드, 어떤 객체를 가지고 있는 것을 말한다.
       @Override
       protected void onRefresh() {
         super.onRefresh();
@@ -40,10 +63,10 @@ public class HellobootApplication {
         webServer.start();
       }
     };
-    applicationContext.registerBean(HelloController.class);
-    applicationContext.registerBean(SimpleHelloService.class);
-    // 여러개의 훅 메소드가 만들어져있다.
-    applicationContext.refresh(); // 스프링 컨테이너 초기화, 템플릿 메소드로 만들어져 있음. (템플릿 메소드는 상속을 통해서 기능을 확장한 의미)
+    // applicationContext.registerBean(HelloController.class); AnnotationConfigWebApplicationContext 에서는 지원하지 않음
+    // applicationContext.registerBean(SimpleHelloService.class);
+    applicationContext.register(HellobootApplication.class); // 자바 코드로 구성된 애노테이션을 직접 등록해야 한다.
+    applicationContext.refresh();
 
 
   }
